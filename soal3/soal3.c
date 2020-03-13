2a1147bf3d07375f6ae7f2664c3bfff46bfab802
Achsanymous <61263465+achsanymous@users.noreply.github.com>
2020-03-13 17:24:15 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -21,7 +21,6 @@ int main(){
 	if (NC2 > 0 && NC3 > 0)
     {
 		while ((wait(&status)) > 0);
-
 		DIR *directory;
 		struct dirent *direct;
 		chdir("/home/achsanymous/Desktop/jpg/");//membuat file jpg
@@ -31,14 +30,11 @@ int main(){
       		while ((direct = readdir(directory)) != NULL){
 	    if(strcmp(direct->d_name, ".") == 0 || strcmp(direct->d_name, "..") == 0)
 		      continue;
-		    if(StatD(direct->d_name))
-{
-		      if(fork() == 0)
-              {
+		    if(StatD(direct->d_name)){
+		      if(fork() == 0) {
 		        char stage[999];
 		        sprintf(stage,"/home/achsanymous/Desktop/jpg/%s",direct->d_name);
-		        char* statement[] = {"mv", stage,"/home/achsanymous/Desktop/indomie/", NULL};//memindahkan direktori
-		        execv("/bin/mv", statement);
+		        char* statement[] = {"mv", stage,"/home/achsanymous/Desktop/indomie/", NULL};execv("/bin/mv", statement);
           		}
           	else{
             	while ((wait(&status)) > 0);
@@ -52,36 +48,29 @@ int main(){
 		        	  	}
               		else{
 		            	while ((wait(&status)) > 0);
-		            	sleep(5);
+		            	sleep(3);
 		            	char stage2[999];
 		            	FILE *clap;
 		            	sprintf(stage2,"/home/achsanymous/Desktop/indomie/%s/coba2.txt",direct->d_name);//membuat txt ke-2
 		            	clap = fopen(stage2, "w");
 		            	fclose(clap);
 		            	exit(0);
-		          		}
-            		}
+		          		}}
             	else{
-				      while ((wait(&status)) > 0);
+				 while ((wait(&status)) > 0);
 					printf("loading . . . ");
 					printf(" ");
 				      exit(0);
-				    } 
-          		} 
-        	}
+				    }}}
         else{
-          	while ((wait(&status)) > 0);
+     		while ((wait(&status)) > 0);
           	if(fork() == 0){
             	while ((wait(&status)) > 0);
             	char stage3[999];
             	sprintf(stage3,"/home/achsanymous/Desktop/jpg/%s",direct->d_name);
             	char* statement[] = {"mv", stage3,"/home/achsanymous/Desktop/sedaap/", NULL};//memindahkan file ke sedaap
             	execv("/bin/mv", statement);
-          		}
-        	}
-      	}
-    	}
-	}
+			}}}}}
 	else if (NC2 == 0 && NC3 > 0){
     while ((wait(&status)) > 0);
     if (NC1 == 0){
@@ -89,16 +78,14 @@ int main(){
       char *statement[] = {"mkdir", "-p","/achsanymous/Desktop/indomie", NULL};//membuat file indomie
       execv("/bin/mkdir", statement);
 	
-    }
+   }
     else{
       while ((wait(&status)) > 0);
 	printf("create file sedaap");
       sleep(5);
       char *statement[] = {"mkdir", "-p","/home/achsanymous/Desktop/sedaap", NULL};//membuat file sedaap
       execv("/bin/mkdir", statement);
-	
-    } 
-  } 
+}} 
 	else if (NC2 > 0 && NC3 == 0){
     char* statement[] = {"unzip", "-oq","/home/achsanymous/Desktop/jpg.zip", NULL};
 	printf("opening package . . .");
