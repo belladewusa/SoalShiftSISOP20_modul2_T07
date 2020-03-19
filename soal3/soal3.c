04ceb618cf966f99ee79254176a84a64168806b3
Achsanymous <61263465+achsanymous@users.noreply.github.com>
2020-03-19 15:38:29 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -6,40 +6,30 @@
 #include <sys/stat.h>
 #include <wait.h>
 
-
-printf("soal3 mulai . . . ");
-
+int StatD(const char *path){
+  struct stat path_Directory;
+  stat(path, &path_Directory);
+  return S_ISDIR(path_Directory.st_mode); //Melakukan cek direktori
+}
 
 int main(){
 	pid_t NC1 = fork();
 	int NC2 = fork(); 
 	int NC3 = fork(); 
-	char stage1[999];
-	char stage2[999];
-	char stage3[999];
-	char direktori1[999];
 	int status; //buffer
-	
-	
-	int StatD(const char *path){
-  struct stat path_Directory;
-  stat(path, &path_Directory);
-  DIR *directory;
-  struct dirent *direct;
-  return S_ISDIR(path_Directory.st_mode); //Melakukan cek direktori
-}
-	
 
 	if (NC2 > 0 && NC3 > 0)
     {
 		while ((wait(&status)) > 0);
-		
+		DIR *directory;
+		struct dirent *direct;
 		chdir("/home/achsanymous/Desktop/jpg/");//membuat file jpg
 		directory = opendir(".");
 
 		if (directory){
       		while ((direct = readdir(directory)) != NULL){
-	    if(strcmp(direct->d_name, ".") == 0 || strcmp(direct->d_name, "..") == 0)continue;
+	    if(strcmp(direct->d_name, ".") == 0 || strcmp(direct->d_name, "..") == 0)
+		      continue;
 		    if(StatD(direct->d_name)){
 		      if(fork() == 0) {
 		        char stage[999];
@@ -48,23 +38,22 @@ int main(){
           		}
           	else{
             	while ((wait(&status)) > 0);
-			
             	if(fork() == 0){
             	  	if(fork() == 0){
-            	    	
-            	    FILE *clap;
-		    sprintf(stage1,"/home/achsanymous/Desktop/indomie/%s/coba1.txt",direct->d_name);//membuat txt ke-1
-		    clap = fopen(stage1, "w");
-		    fclose(clap);
-				printf("create txt1  ");
+            	    	char stage1[999];
+            	    	FILE *clap;
+		        	    sprintf(stage1,"/home/achsanymous/Desktop/indomie/%s/coba1.txt",direct->d_name);//membuat txt ke-1
+		        	    clap = fopen(stage1, "w");
+		        	    fclose(clap);
 		        	  	}
               		else{
 		            	while ((wait(&status)) > 0);
-		            	sleep(7);
+		            	sleep(3);
+		            	char stage2[999];
 		            	FILE *clap;
 		            	sprintf(stage2,"/home/achsanymous/Desktop/indomie/%s/coba2.txt",direct->d_name);//membuat txt ke-2
-		            	clap = fopen(stage2, "w");fclose(clap);
-				printf("create txt2");
+		            	clap = fopen(stage2, "w");
+		            	fclose(clap);
 		            	exit(0);
 		          		}}
             	else{
@@ -77,31 +66,29 @@ int main(){
      		while ((wait(&status)) > 0);
           	if(fork() == 0){
             	while ((wait(&status)) > 0);
+            	char stage3[999];
             	sprintf(stage3,"/home/achsanymous/Desktop/jpg/%s",direct->d_name);
             	char* statement[] = {"mv", stage3,"/home/achsanymous/Desktop/sedaap/", NULL};//memindahkan file ke sedaap
             	execv("/bin/mv", statement);
-		printf("moving file");
 			}}}}}
 	else if (NC2 == 0 && NC3 > 0){
     while ((wait(&status)) > 0);
     if (NC1 == 0){
 	printf("create file indomie");
-	    sleep(2);
-      char *statement[] = {"mkdir", "-p","/achsanymous/Desktop/sedaap", NULL};//membuat file indomie
+      char *statement[] = {"mkdir", "-p","/achsanymous/Desktop/indomie", NULL};//membuat file indomie
       execv("/bin/mkdir", statement);
 	
    }
     else{
       while ((wait(&status)) > 0);
 	printf("create file sedaap");
-      sleep(2);
-      char *statement[] = {"mkdir", "-p","/home/achsanymous/Desktop/indomie", NULL};//membuat file sedaap
+      sleep(5);
+      char *statement[] = {"mkdir", "-p","/home/achsanymous/Desktop/sedaap", NULL};//membuat file sedaap
       execv("/bin/mkdir", statement);
 }} 
 	else if (NC2 > 0 && NC3 == 0){
     char* statement[] = {"unzip", "-oq","/home/achsanymous/Desktop/jpg.zip", NULL};
 	printf("opening package . . .");
-		sleep(2);
     execv("/usr/bin/unzip", statement);
 	}
 	return 0;
